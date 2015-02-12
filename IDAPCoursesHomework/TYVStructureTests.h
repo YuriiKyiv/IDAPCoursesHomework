de264bfc4c8eca7068d28baae42bd98cf14ad815
YURII <yurii@YURIIs-iMac.local>
2015-02-12 14:32:37 +0200

    add SizeTest

---
+++ IDAPCoursesHomework/TYVStructureTests.h
@@ -0,0 +1,16 @@
+//
+//  TYVStructureTests.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 12.02.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVStructureTests__
+#define __IDAPCoursesHomework__TYVStructureTests__
+
+#include <stdio.h>
+
+void TYVStrucureSizeTest()
+
+#endif /* defined(__IDAPCoursesHomework__TYVStructureTests__) */
