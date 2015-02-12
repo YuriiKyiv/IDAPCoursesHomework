e2f872d563b20c5bfbb588251ab4d8f12f2544be
YURII <yurii@YURIIs-iMac.local>
2015-02-12 15:55:08 +0200

    task 2

--- IDAPCoursesHomework/main.c
+++ IDAPCoursesHomework/main.c
@@ -7,9 +7,32 @@
 //
 
 #include <stdio.h>
+#include <assert.h>
+
+#define TYVValueGenerete(type, spec) \
+type TYVOutput_##type(type value) { \
+printf("%"#spec, value); \
+return value; \
+}
+
+#define TYVOutputValue(type, value) \
+TYVOutput_##type(value); \
+
+TYVValueGenerete(int, d);
+TYVValueGenerete(long, ld);
+TYVValueGenerete(char, c);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    
+    TYVOutputValue(int, 15)
+    TYVOutputValue(long, 100500)
+    TYVOutputValue(char,'c')
+    
+    assert(12 == TYVOutput_int(12));
+    assert(100500 == TYVOutput_long(100500));
+    assert('c'== TYVOutput_char('c'));
+    
     return 0;
 }
