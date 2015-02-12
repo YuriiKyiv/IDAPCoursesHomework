32fab55449172d8b0a9587432bb969621921cfd1
YURII <yurii@YURIIs-iMac.local>
2015-02-12 14:25:52 +0200

    init

---
+++ IDAPCoursesHomework/TYVStructure.h
@@ -0,0 +1,14 @@
+//
+//  TYVStructure.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 12.02.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVStructure__
+#define __IDAPCoursesHomework__TYVStructure__
+
+#include <stdio.h>
+
+#endif /* defined(__IDAPCoursesHomework__TYVStructure__) */
