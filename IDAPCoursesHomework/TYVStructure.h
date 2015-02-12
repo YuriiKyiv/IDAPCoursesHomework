26a8323c6e70ffedae16e71c239a9a61924c96d5
YURII <yurii@YURIIs-iMac.local>
2015-02-12 14:30:03 +0200

    created the structure

--- IDAPCoursesHomework/TYVStructure.h
+++ IDAPCoursesHomework/TYVStructure.h
@@ -10,5 +10,33 @@
 #define __IDAPCoursesHomework__TYVStructure__
 
 #include <stdio.h>
+#include "stdbool.h"
+
+struct TYVBasicStucture {
+    
+    bool bool1;
+    bool bool2;
+    bool bool3;
+    bool bool4;
+    bool bool5;
+    bool bool6;
+    
+    float float1;
+    
+    int int1;
+    
+    long long longLong1;
+    
+    short short1;
+    short short2;
+    short short3;
+    
+    double double1;
+    
+    char *char1;
+    
+};
+
+typedef struct TYVBasicStucture TYVBasicStucture;
 
 #endif /* defined(__IDAPCoursesHomework__TYVStructure__) */
