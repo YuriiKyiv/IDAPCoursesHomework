1ff78782742f0da3f5f116d67f753d8101a93a33
YURII <yurii@YURIIs-iMac.local>
2015-02-16 11:18:44 +0200

    add second condition

--- IDAPCoursesHomework/TYVValueBitOutput.c
+++ IDAPCoursesHomework/TYVValueBitOutput.c
@@ -9,18 +9,28 @@
 #include "TYVValueBitOutput.h"
 #include "stdbool.h"
 
+enum endian {litleEndian = 1, bigEndian = 0};
+
 void IDPValueBitOutput(void *valueRef, size_t size, bool endian) {
-    if (0 == endian) {
-        unsigned char *charValueRef = (unsigned char *)valueRef;
-        unsigned char charValue = 0;
-    
+    unsigned char *charValueRef = (unsigned char *)valueRef;
+    unsigned char charValue = 0;
+    if (litleEndian == endian) {
         for (long int byteIter = size - 1; byteIter >= 0; byteIter--) {
             charValue = charValueRef[byteIter];
             for (int bitIter = 0; bitIter < 8; bitIter++) {
                 printf("%u", 0 != (charValue & 128));
                 charValue <<= 1;
             }
-        }
+    }
+        
+    } else {
+            for (long int byteIter = 0; byteIter < size; byteIter++) {
+                charValue = charValueRef[byteIter];
+                for (int bitIter = 0; bitIter < 8; bitIter++) {
+                    printf("%u", 0 != (charValue & 128));
+                    charValue <<= 1;
+                }
+            }
     }
     
     printf("\n");
