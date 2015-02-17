ad8f5ce291d6fef63c5042294821d1cbfc5b44f5
YURII <yurii@YURIIs-iMac.local>
2015-02-17 12:57:18 +0200

    rewrite loops

--- IDAPCoursesHomework/TYVValueBitOutput.c
+++ IDAPCoursesHomework/TYVValueBitOutput.c
@@ -9,27 +9,30 @@
 #include "TYVValueBitOutput.h"
 #include "stdbool.h"
 
+void TYVBitLoop(unsigned char charValue);
+void TYVByteLoop(unsigned char charValue, unsigned char *charValueRef, size_t size, size_t start, size_t end);
+
 void TYVValueBitOutput(void *valueRef, size_t size, bool endian) {
     unsigned char *charValueRef = (unsigned char *)valueRef;
     unsigned char charValue = 0;
-    if (bigEndian == endian) {
-        for (long int byteIter = size - 1; byteIter >= 0; byteIter--) {
-            charValue = charValueRef[byteIter];
-            for (int bitIter = 0; bitIter < 8; bitIter++) {
-                printf("%u", 0 != (charValue & 128));
-                charValue <<= 1;
-            }
-    }
-        
-    } else {
-            for (long int byteIter = 0; byteIter < size; byteIter++) {
-                charValue = charValueRef[byteIter];
-                for (int bitIter = 0; bitIter < 8; bitIter++) {
-                    printf("%u", 0 != (charValue & 128));
-                    charValue <<= 1;
-                }
-            }
-    }
-    
+    endian ?
+        TYVByteLoop(charValue, charValueRef, size, 0, size) :
+        TYVByteLoop(charValue, charValueRef, size, size-1, 0);
     printf("\n");
+}
+
+inline
+void TYVByteLoop(unsigned char charValue, unsigned char *charValueRef, size_t size, size_t start, size_t end){
+    for (long int byteIter = start; byteIter < end; byteIter++) {
+        charValue = charValueRef[byteIter];
+        TYVBitLoop(charValue);
+    }
+}
+
+inline
+void TYVBitLoop(unsigned char charValue){
+    for (int bitIter = 0; bitIter < 8; bitIter++) {
+        printf("%u", 0 != (charValue & 128));
+        charValue <<= 1;
+    }
 }
