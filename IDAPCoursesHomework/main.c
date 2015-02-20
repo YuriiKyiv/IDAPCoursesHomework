895bc9e0c1fd2bafd4300532972bb4266a6c6774
YURII <yurii@YURIIs-iMac.local>
2015-02-20 18:23:24 +0200

    delete method name

--- IDAPCoursesHomework/main.c
+++ IDAPCoursesHomework/main.c
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     
-    TYVStuctureTestsRunAll();
+    TYVStuctureTestsRun();
     
     return 0;
 }
