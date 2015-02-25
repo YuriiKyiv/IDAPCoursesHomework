c6174e8fe0b126af38561af7ec68ca93ac8b7174
YURII <trohumchyk@gmail.com>
2015-02-25 12:16:02 +0200

    fix task 4

--- IDAPCoursesHomework/TYVValueBitOutput.c
+++ IDAPCoursesHomework/TYVValueBitOutput.c
@@ -15,8 +15,7 @@ void TYVValueBitsOutput(void *valueRef, size_t size, TYVEndian endian) {
     size_t iter = 0;
     for (long int byteIter = 0; byteIter < size; byteIter++) {
         iter = endian ? byteIter : (size - byteIter - 1);
-        unsigned char charValue = charValueRef[iter];
-        TYVByteLoop(charValue);
+        TYVByteLoop(charValueRef[iter]);
         printf(" ");
     }
     
