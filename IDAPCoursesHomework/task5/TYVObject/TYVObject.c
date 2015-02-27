bb1b870f560e7d54f687ab91445c5fb6eafd5c92
YURII <trohumchyk@gmail.com>
2015-02-27 14:10:35 +0200

    init TYVObject

---
+++ IDAPCoursesHomework/task5/TYVObject/TYVObject.c
@@ -0,0 +1,9 @@
+//
+//  TYVObject.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 27.02.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVObject.h"
---
+++ IDAPCoursesHomework/task5/TYVObject/TYVObject.h
@@ -0,0 +1,14 @@
+//
+//  TYVObject.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 27.02.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVObject__
+#define __IDAPCoursesHomework__TYVObject__
+
+#include <stdio.h>
+
+#endif /* defined(__IDAPCoursesHomework__TYVObject__) */
