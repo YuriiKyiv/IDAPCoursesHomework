0b2801b3a5d648fb614bc1f7905627a710750642
YURII <trohumchyk@gmail.com>
2015-03-02 14:01:51 +0200

    declaretion array

---
+++ IDAPCoursesHomework/task5/TYVArray/TYVArray.c
@@ -0,0 +1,9 @@
+//
+//  TYVArray.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 02.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVArray.h"
---
+++ IDAPCoursesHomework/task5/TYVArray/TYVArray.h
@@ -0,0 +1,27 @@
+//
+//  TYVArray.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 02.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVArray__
+#define __IDAPCoursesHomework__TYVArray__
+
+#include <stdio.h>
+#include "TYVObject.h"
+
+#endif /* defined(__IDAPCoursesHomework__TYVArray__) */
+
+typedef struct TYVArray TYVArray;
+struct TYVArray {
+    TYVObject _object;
+    
+    void *_array[20];
+    uint8_t _count;
+};
+
+void TYVArrayAdd(TYVArray *array, void *);
+
+void TYVArrayRemove(TYVArray *array, void *);
