7ea811c0e423f9069c05b39005f450276afa6d11
YURII <trohumchyk@gmail.com>
2015-03-11 16:53:12 +0200

    start to write array list

--- IDAPCoursesHomework/task5/TYVArrayList/TYVArrayList.c
+++ IDAPCoursesHomework/task5/TYVArrayList/TYVArrayList.c
@@ -28,91 +28,41 @@ size_t TYVArrayListGetSize(TYVArrayList *array);
 #pragma mark Public Implementations
 
 TYVArrayList *TYVArrayListCreate(size_t size){
-    if (0 == size){
-        size = 1;
-    }
-    TYVArrayList *arrayList = TYVObjectCreate(TYVArrayList);
-    void *newData = calloc(size, sizeof(*newData));
-    TYVArrayListSetData(arrayList, newData);
-    TYVArrayListSetSize(arrayList, size);
+    TYVArrayList *array = TYVObjectCreate(TYVArrayList);
+    TYVArrayListSetSize(array, size);
     
-    return arrayList;
-}
-
-
-
-void __TYVArrayListDeallocate(TYVArrayList *arrayList){
-    
+    return array;
 }
 
 uint64_t TYVArrayListGetCount(TYVArrayList *array){
-    return (NULL != array) ? array->_count : 0;
-}
-
-size_t TYVArrayListGetSize(TYVArrayList *array){
-    return (NULL != array) ? array->_size : 0;
-}
-
-void TYVArrayListAddItem(TYVArrayList *array, void *item){
-    if (NULL == array){
-        return;
-    }
-    
-    uint64_t currentCount = TYVArrayListGetCount(array);
-    size_t arraySize = TYVArrayListGetSize(array);
-    
-    if (arraySize == currentCount){
-        TYVArrayListResize(array);
-    }
-    
-    void *data = TYVArrayListGetData(array);
-    size_t iter = 0;
-    while ((NULL != &data[iter]) && (iter < arraySize)) {
-        iter++;
-    }
-    
-    TYVArrayListSetCount(array, currentCount + 1);
-    //data[iter] = *item;
+    return array->_count;
 }
 
 #pragma mark -
 #pragma mark Private Implementations
 
 void TYVArrayListSetData(TYVArrayList *arrayList, void *newData){
-    if (NULL == arrayList || arrayList->_data == newData){
+    if (NULL == arrayList){
         return;
     }
     
-    arrayList->_data = newData;
-}
-
-void *TYVArrayListGetData(TYVArrayList *array){
-    return (NULL != array) ? array->_data : NULL;
+    
 }
 
 void TYVArrayListSetSize(TYVArrayList *array, size_t newSize){
-    if (NULL == array || array->_size == newSize){
+    size_t currentSize = array->_size;
+    void *data = TYVArrayListGetData(array);
+    if (NULL == array || currentSize){
         return;
     }
     
-    array->_size = newSize;
+    if (currentSize < newSize){
+        data = realloc(data, newSize);
+    }
+    
+    
 }
 
-void TYVArrayListSetCount(TYVArrayList *array, uint64_t newCount){
-    if (NULL == array || array->_count == newCount){
-        return;
-    }
-    
-    array->_count = newCount;
-}
-
-void TYVArrayListResize(TYVArrayList *array){
-    if (NULL == array){
-        return;
-    }
-    
-    size_t newSize = TYVArrayListGetSize(array)*2;
-    void *newData = realloc(TYVArrayListGetData(array), newSize);
-    TYVArrayListSetData(array, newData);
-    TYVArrayListSetSize(array, newSize);
+size_t TYVArrayListGetSize(TYVArrayList *array){
+    return array->_size;
 }
