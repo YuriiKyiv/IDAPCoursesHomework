292665562aa121814cbc64bbb11a2f65e88317cf
YURII <trohumchyk@gmail.com>
2015-03-13 16:04:00 +0200

    init TYVUniversalSetters

---
+++ IDAPCoursesHomework/task5/TYVUniversalSetters/TYVUniversalSetters.c
@@ -0,0 +1,9 @@
+//
+//  TYVUniversalSetters.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 13.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVUniversalSetters.h"
---
+++ IDAPCoursesHomework/task5/TYVUniversalSetters/TYVUniversalSetters.h
@@ -0,0 +1,14 @@
+//
+//  TYVUniversalSetters.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 13.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVUniversalSetters__
+#define __IDAPCoursesHomework__TYVUniversalSetters__
+
+#include <stdio.h>
+
+#endif /* defined(__IDAPCoursesHomework__TYVUniversalSetters__) */
