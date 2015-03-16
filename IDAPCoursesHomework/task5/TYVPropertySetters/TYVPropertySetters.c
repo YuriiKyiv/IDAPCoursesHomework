0595089424a7b757cc433efa91aff6860b8018c5
YURII <trohumchyk@gmail.com>
2015-03-16 13:12:04 +0200

    new propertySetters

---
+++ IDAPCoursesHomework/task5/TYVPropertySetters/TYVPropertySetters.c
@@ -0,0 +1,34 @@
+//
+//  TYVPropertySetters.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 16.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVPropertySetters.h"
+#include "TYVObject.h"
+
+void TYVPropertySetRetain(void **field, void *value){
+    if (NULL == field || *field == value){
+        return;
+    }
+    
+    if (NULL != *field){
+        TYVObjectRelease(*field);
+    }
+    
+    if (NULL != value){
+        TYVObjectRetain(value);
+    }
+    
+    *field = value;
+}
+
+void TYVPropertySetAssign(void **field, void *value){
+    if (NULL == field || *field == value){
+        return;
+    }
+    
+    *field = value;
+}
---
+++ IDAPCoursesHomework/task5/TYVPropertySetters/TYVPropertySetters.h
@@ -0,0 +1,18 @@
+//
+//  TYVPropertySetters.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 16.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVPropertySetters__
+#define __IDAPCoursesHomework__TYVPropertySetters__
+
+#include <stdio.h>
+
+void TYVPropertySetRetain(void **field, void *value);
+
+void TYVPropertySetAssign(void **field, void *value);
+
+#endif /* defined(__IDAPCoursesHomework__TYVPropertySetters__) */
