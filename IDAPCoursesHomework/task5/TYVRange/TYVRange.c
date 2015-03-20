9123d403066a1b9f844b0f807d83abd246a11daa
YURII <trohumchyk@gmail.com>
2015-03-20 14:09:50 +0200

    init TYVRange

---
+++ IDAPCoursesHomework/task5/TYVRange/TYVRange.c
@@ -0,0 +1,9 @@
+//
+//  TYVRange.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 20.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVRange.h"
---
+++ IDAPCoursesHomework/task5/TYVRange/TYVRange.h
@@ -0,0 +1,14 @@
+//
+//  TYVRange.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 20.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVRange__
+#define __IDAPCoursesHomework__TYVRange__
+
+#include <stdio.h>
+
+#endif /* defined(__IDAPCoursesHomework__TYVRange__) */
