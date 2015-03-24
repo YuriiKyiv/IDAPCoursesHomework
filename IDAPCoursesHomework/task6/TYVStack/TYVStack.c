56e926775c664b369c56018bba928b590978476d
YURII <trohumchyk@gmail.com>
2015-03-24 13:09:26 +0200

    TYVStack init

---
+++ IDAPCoursesHomework/task6/TYVStack/TYVStack.c
@@ -0,0 +1,9 @@
+//
+//  TYVStack.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 24.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVStack.h"
---
+++ IDAPCoursesHomework/task6/TYVStack/TYVStack.h
@@ -0,0 +1,14 @@
+//
+//  TYVStack.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 24.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVStack__
+#define __IDAPCoursesHomework__TYVStack__
+
+#include <stdio.h>
+
+#endif /* defined(__IDAPCoursesHomework__TYVStack__) */
