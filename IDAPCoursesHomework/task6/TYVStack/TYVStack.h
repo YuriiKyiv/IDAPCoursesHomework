b8c5b76e5100d727a34d9b90b52ad015e2fedd36
YURII <trohumchyk@gmail.com>
2015-03-24 13:39:54 +0200

    public diclaretion

--- IDAPCoursesHomework/task6/TYVStack/TYVStack.h
+++ IDAPCoursesHomework/task6/TYVStack/TYVStack.h
@@ -10,5 +10,33 @@
 #define __IDAPCoursesHomework__TYVStack__
 
 #include <stdio.h>
+#include "TYVObject.h"
+#include <stdbool.h>
+
+typedef struct TYVStack TYVStack;
+struct TYVStack {
+    TYVObject _super;
+    
+    TYVObject **_data;
+    
+    size_t _size;
+    size_t _currentSize;
+};
+
+extern
+void TYVStackPushItem(TYVStack *stack, TYVObject *data);
+
+extern
+TYVObject *TYVStackPopItem(TYVStack *stack);
+
+extern
+bool TYVStackIsFull(TYVStack *stack);
+
+extern
+bool TYVStackIsEmpty(TYVStack *stack);
+
+//bool TYVStackIsFull(TYVStack *stack){
+//    return (NULL != stack) ? stack->_size == stack->_currentSize : false;
+//}
 
 #endif /* defined(__IDAPCoursesHomework__TYVStack__) */
