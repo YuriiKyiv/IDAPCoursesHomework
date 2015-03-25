4cbe796bb317121f9e9a3bd2867958e0e184d407
YURII <trohumchyk@gmail.com>
2015-03-25 13:06:41 +0200

    TYVAutoReleaseStack init

---
+++ IDAPCoursesHomework/task6/TYVStack/TYVAutoReleaseStack.c
@@ -0,0 +1,9 @@
+//
+//  TYVAutoReleaseStack.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 25.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVAutoReleaseStack.h"
---
+++ IDAPCoursesHomework/task6/TYVStack/TYVAutoReleaseStack.h
@@ -0,0 +1,14 @@
+//
+//  TYVAutoReleaseStack.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 25.03.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#ifndef __IDAPCoursesHomework__TYVAutoReleaseStack__
+#define __IDAPCoursesHomework__TYVAutoReleaseStack__
+
+#include <stdio.h>
+
+#endif /* defined(__IDAPCoursesHomework__TYVAutoReleaseStack__) */
