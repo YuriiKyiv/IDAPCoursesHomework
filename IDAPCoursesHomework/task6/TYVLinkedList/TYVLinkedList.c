e2e2d3c8a5f1dfc073295a9c80053bf7f15ce850
YURII <trohumchyk@gmail.com>
2015-03-26 14:56:26 +0200

    TYVLinkedListAddObject

--- IDAPCoursesHomework/task6/TYVLinkedList/TYVLinkedList.c
+++ IDAPCoursesHomework/task6/TYVLinkedList/TYVLinkedList.c
@@ -7,6 +7,17 @@
 //
 
 #include "TYVLinkedList.h"
+#include "TYVLinkedListNode.h"
+
+#pragma mark -
+#pragma mark Private Declarations
+
+void TYVLinkedListSetRootNode(TYVLinkedList *list, TYVLinkedListNode *node);
+
+TYVLinkedListNode *TYVLinkedListGetRootNode(TYVLinkedList *list);
+
+#pragma mark -
+#pragma mark Public Implementations
 
 TYVLinkedList *TYVLinkedListCreate(){
     return TYVObjectCreate(TYVLinkedList);
@@ -17,3 +28,42 @@ void __TYVLinkedListDeallocate(TYVLinkedList *list){
     
     __TYVObjectDeallocate(list);
 }
+
+void TYVLinkedListAddObject(TYVLinkedList *list, TYVObject *object) {
+    if (NULL == list || NULL == object) {
+        return;
+    }
+    
+    TYVLinkedListNode *rootNode = TYVLinkedListGetRootNode(list);
+    TYVLinkedListNode *newNode = TYVLinkedListNodeCreateWithObjectAndNextNode(object, rootNode);
+    TYVLinkedListSetRootNode(list, newNode);
+    
+    list->_count++;
+    
+    TYVObjectRelease(newNode);
+    
+}
+
+extern
+void TYVLinkedListRemoveObject(TYVLinkedList *list, TYVObject *object);
+
+extern
+void TYVLinkedListRemoveAllObjects(TYVLinkedList *list);
+
+extern
+void TYVLinkedListContainsObject(TYVLinkedList *list, TYVObject *object);
+
+extern
+void TYVLinkedListInsertBeforeObject(TYVLinkedList *list, TYVObject *insertionPoint, TYVObject *object);
+
+extern
+void TYVLinkedListInsertAfterObject(TYVLinkedList *list, TYVObject *insertionPoint, TYVObject *object);
+
+extern
+TYVLinkedListNode *TYVLinkedListGetFirstObject(TYVLinkedList *list);
+
+extern
+uint64_t TYVLinkedListGetCount(TYVLinkedList *list);
+
+#pragma mark -
+#pragma mark Private Implementations
