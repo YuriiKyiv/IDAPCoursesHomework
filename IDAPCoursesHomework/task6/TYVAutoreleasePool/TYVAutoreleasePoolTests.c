4cd46a7220361f9879f5650e8123f27e93bd6ea2
YURII <trohumchyk@gmail.com>
2015-04-04 21:36:59 +0300

    TYVAutoreleasePoolTests init

---
+++ IDAPCoursesHomework/task6/TYVAutoreleasePool/TYVAutoreleasePoolTests.c
@@ -0,0 +1,9 @@
+//
+//  TYVAutoreleasePoolTests.c
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 04.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#include "TYVAutoreleasePoolTests.h"
