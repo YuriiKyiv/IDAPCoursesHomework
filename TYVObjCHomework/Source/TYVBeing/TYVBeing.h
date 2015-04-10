3b6a7abe0357eddf529261457738e0977fc082f0
YURII <trohumchyk@gmail.com>
2015-04-10 20:08:23 +0300

    TYVBeing declaretion

--- TYVObjCHomework/Source/TYVBeing/TYVBeing.h
+++ TYVObjCHomework/Source/TYVBeing/TYVBeing.h
@@ -8,6 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum TYVGender TYVGender;
+enum TYVGender {
+    TYVMaleGender,
+    TYVFemaleGender
+};
+
 @interface TYVBeing : NSObject
+
+@property (nonatomic, copy, readonly)   NSString    *name;
+@property (nonatomic, copy, readonly)   NSArray     *chidren;
+
+@property (nonatomic, assign, readonly) TYVGender   gender;
+@property (nonatomic, assign, readonly) uint8_t     weight;
+@property (nonatomic, assign, readonly) uint8_t     age;
+
+- (void)fight;
+
+- (TYVBeing *)giveBirth;
+
+- (void)addChild:(TYVBeing *)aChild;
+
+- (void)removeChild:(TYVBeing *)aChild;
+
+- (void)sayHi;
 
 @end
