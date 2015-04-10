24d1023bf18a6e5c2c0b3d1ad01ac455a8889720
YURII <trohumchyk@gmail.com>
2015-04-10 19:39:13 +0300

    TYVBeing - init

---
+++ TYVObjCHomework/Source/TYVBeing/TYVBeing.h
@@ -0,0 +1,13 @@
+//
+//  TYVBeing.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 10.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVBeing : NSObject
+
+@end
---
+++ TYVObjCHomework/Source/TYVBeing/TYVBeing.m
@@ -0,0 +1,13 @@
+//
+//  TYVBeing.m
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 10.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVBeing.h"
+
+@implementation TYVBeing
+
+@end
