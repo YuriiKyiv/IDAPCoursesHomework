9cb37bb18dd3d267b1b5c7ba367f36c422285b74
YURII <trohumchyk@gmail.com>
2015-04-11 21:36:36 +0300

    add private properties

--- TYVObjCHomework/Source/TYVBeing/TYVBeing.m
+++ TYVObjCHomework/Source/TYVBeing/TYVBeing.m
@@ -8,6 +8,49 @@
 
 #import "TYVBeing.h"
 
+@interface TYVBeing ()
+
+@property (nonatomic, copy)   NSString    *name;
+@property (nonatomic, copy)   NSArray     *chidren;
+
+@property (nonatomic, assign) TYVGender   gender;
+@property (nonatomic, assign) uint8_t     weight;
+@property (nonatomic, assign) uint8_t     age;
+
+- (void)sayMessage:(NSString *)aMessage;
+
+@end
+
 @implementation TYVBeing
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)fight {
+    [self sayMessage:@"fight"];
+}
+
+- (TYVBeing *)giveBirth {
+    return [[[TYVBeing alloc] init] autorelease];
+}
+
+- (void)addChild:(TYVBeing *)aChild {
+    [self sayMessage:@"add"];
+}
+
+- (void)removeChild:(TYVBeing *)aChild {
+    [self sayMessage:@"remove"];
+}
+
+- (void)sayHi {
+    [self sayMessage:@"Hi"];
+}
+
+#pragma mark -
+#pragma mark Private Methods
+
+- (void)sayMessage:(NSString *)aMessage {
+    NSLog(@"%@", aMessage);
+}
 
 @end
