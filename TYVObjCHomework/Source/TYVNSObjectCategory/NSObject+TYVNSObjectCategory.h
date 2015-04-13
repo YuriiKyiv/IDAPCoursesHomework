0f48b64028e432ab84a6734deb94079217b41da3
YURII <trohumchyk@gmail.com>
2015-04-13 22:11:12 +0300

    init category

---
+++ TYVObjCHomework/Source/TYVNSObjectCategory/NSObject+TYVNSObjectCategory.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+TYVNSObjectCategory.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 13.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (TYVNSObjectCategory)
+
+@end
