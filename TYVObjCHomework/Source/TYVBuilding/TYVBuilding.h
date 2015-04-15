e1065f2dc1a453515e63d557d44841fa61c01e2f
YURII <trohumchyk@gmail.com>
2015-04-15 13:40:27 +0300

    ini Building

---
+++ TYVObjCHomework/Source/TYVBuilding/TYVBuilding.h
@@ -0,0 +1,14 @@
+//
+//  TYVBuilding.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 15.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVBuilding : NSObject
+@property (nonatomic, copy, readonly)   NSArray *rooms;
+
+@end
