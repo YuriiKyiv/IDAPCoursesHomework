f3e6574022d92d97b4c562964c03f2c4e5dbf0ba
YURII <trohumchyk@gmail.com>
2015-04-15 13:40:49 +0300

    init room

---
+++ TYVObjCHomework/Source/TYVRoom/TYVRoom.h
@@ -0,0 +1,15 @@
+//
+//  TYVRoom.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 15.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVRoom : NSObject
+@property   (nonatomic, assign, readonly)   uint8_t maxHumanCount;
+@property   (nonatomic, assign, readonly)   uint8_t currentHumanCount;
+
+@end
---
+++ TYVObjCHomework/Source/TYVRoom/TYVRoom.m
@@ -0,0 +1,13 @@
+//
+//  TYVRoom.m
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 15.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVRoom.h"
+
+@implementation TYVRoom
+
+@end
