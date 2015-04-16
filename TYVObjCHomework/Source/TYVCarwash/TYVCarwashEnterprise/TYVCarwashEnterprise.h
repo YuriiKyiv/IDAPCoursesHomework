b9aa9c32b830679e5e7105fcab6faf8eb6b3bca6
YURII <trohumchyk@gmail.com>
2015-04-16 16:20:10 +0300

    add TYVCarwashEnterprise

---
+++ TYVObjCHomework/Source/TYVCarwash/TYVCarwashEnterprise/TYVCarwashEnterprise.h
@@ -0,0 +1,13 @@
+//
+//  TYVCarwashEnterprise.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 16.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVCarwashEnterprise : NSObject
+
+@end
