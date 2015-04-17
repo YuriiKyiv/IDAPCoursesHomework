165235cdc3656dc30ce8abf7867de87aa18e9ece
YURII <trohumchyk@gmail.com>
2015-04-17 20:42:52 +0300

    TYVCar

---
+++ TYVObjCHomework/Source/TYVNSObjectExtensions/NSObject+TYVNSObjectExtensions.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+TYVNSObjectExtensions.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 17.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (TYVNSObjectExtensions)
+
+@end
---
+++ TYVObjCHomework/Source/TYVNSObjectExtensions/NSObject+TYVNSObjectExtensions.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+TYVNSObjectExtensions.m
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 17.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "NSObject+TYVNSObjectExtensions.h"
+
+@implementation NSObject (TYVNSObjectExtensions)
+
+@end
