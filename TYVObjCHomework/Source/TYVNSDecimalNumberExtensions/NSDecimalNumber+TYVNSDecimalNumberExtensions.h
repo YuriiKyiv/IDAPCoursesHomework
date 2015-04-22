cd5f37b96103a179187f59da50a24ae0a07ec9db
YURII <trohumchyk@gmail.com>
2015-04-22 15:03:21 +0300

    TYVNSDecimalNumberExtensions

---
+++ TYVObjCHomework/Source/TYVNSDecimalNumberExtensions/NSDecimalNumber+TYVNSDecimalNumberExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSDecimalNumber+TYVNSDecimalNumberExtensions.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 22.04.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDecimalNumber (TYVNSDecimalNumberExtensions)
+
++ (id)number:(NSString *)number;
+
+@end
