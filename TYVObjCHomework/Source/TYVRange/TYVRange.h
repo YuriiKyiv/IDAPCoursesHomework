a28c4ea74cc39731fac99cbea0f7deb0e2c170a8
YURII <trohumchyk@gmail.com>
2015-05-04 14:49:14 +0300

    TYVRange Inint

---
+++ TYVObjCHomework/Source/TYVRange/TYVRange.h
@@ -0,0 +1,13 @@
+//
+//  TYVRange.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 04.05.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVRange : NSObject
+
+@end
