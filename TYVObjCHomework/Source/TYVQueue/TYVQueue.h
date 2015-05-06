9c7e8fb1c50bab675166af366661b4efdcfdc73a
YURII <trohumchyk@gmail.com>
2015-05-06 12:57:08 +0300

    init TYVQueue

---
+++ TYVObjCHomework/Source/TYVQueue/TYVQueue.h
@@ -0,0 +1,13 @@
+//
+//  TYVQueue.h
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 06.05.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVQueue : NSObject
+
+@end
---
+++ TYVObjCHomework/Source/TYVQueue/TYVQueue.m
@@ -0,0 +1,13 @@
+//
+//  TYVQueue.m
+//  IDAPCoursesHomework
+//
+//  Created by YURII on 06.05.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVQueue.h"
+
+@implementation TYVQueue
+
+@end
