695de904eba76528e00888089ea22dbdc422a0e1
YURII <trohumchyk@gmail.com>
2015-05-07 13:22:48 +0300

    TYVQueue

--- TYVObjCHomework/Source/TYVQueue/TYVQueue.m
+++ TYVObjCHomework/Source/TYVQueue/TYVQueue.m
@@ -8,6 +8,53 @@
 
 #import "TYVQueue.h"
 
+@interface TYVQueue ()
+@property (nonatomic, retain)   NSMutableArray  *queue;
+
+@end
+
 @implementation TYVQueue
+
+@dynamic empty;
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.queue = nil;
+    
+    [super dealloc];
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.queue = [NSMutableArray array];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (BOOL)isEmpty {
+    return [self.queue count];
+}
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)enqueueObject:(id)object {
+    [self.queue addObject:object];
+}
+
+- (id)dequeueObject {
+    NSMutableArray *array = self.queue;
+    id result = array[0];
+    [array removeObjectAtIndex:0];
+    
+    return result;
+}
 
 @end
