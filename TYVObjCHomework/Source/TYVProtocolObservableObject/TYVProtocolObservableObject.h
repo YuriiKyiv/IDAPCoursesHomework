de41102c1fc916e7186be54b124b491869977f01
Yurii Trokhymchuk <trohumchyk@gmail.com>
2015-05-14 20:21:22 +0300

    init TYVProtocolObservableObject

---
+++ TYVObjCHomework/Source/TYVProtocolObservableObject/TYVProtocolObservableObject.h
@@ -0,0 +1,23 @@
+//
+//  TYVProtocolObservableObject.h
+//  IDAPCoursesHomework
+//
+//  Created by Yurii Trokhymchuk on 5/14/15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVProtocolObservableObject : NSObject
+
+@property (nonatomic, readonly) NSSet   *observersSet;
+
+- (void)addObserver:(id)observer;
+
+- (void)removeObserver:(id)observer;
+
+- (BOOL)containsObserver:(id)observer;
+
+- (SEL)selectorForState:(NSUInteger)state;
+
+@end
---
+++ TYVObjCHomework/Source/TYVProtocolObservableObject/TYVProtocolObservableObject.m
@@ -0,0 +1,80 @@
+//
+//  TYVProtocolObservableObject.m
+//  IDAPCoursesHomework
+//
+//  Created by Yurii Trokhymchuk on 5/14/15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVProtocolObservableObject.h"
+
+@interface TYVProtocolObservableObject ()
+
+@property (nonatomic, retain) NSHashTable   *observersHashTable;
+
+@end
+
+@implementation TYVProtocolObservableObject
+
+@dynamic observersSet;
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.observersHashTable = nil;
+    
+    [super dealloc];
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.observersHashTable = [NSHashTable weakObjectsHashTable];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSSet *)observersSet {
+    return self.observersHashTable.setRepresentation;
+}
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)addObserver:(id)observer {
+    [self.observersHashTable addObject:observer];
+}
+
+- (void)removeObserver:(id)observer {
+    [self.observersHashTable removeObject:observer];
+}
+
+- (BOOL)containsObserver:(id)observer {
+    return [self.observersHashTable containsObject:observer];
+    
+}
+
+- (SEL)selectorForState:(NSUInteger)state {
+    [self doesNotRecognizeSelector:_cmd];
+    
+    return nil;
+}
+
+#pragma mark -
+#pragma mark Private Methods
+
+- (void)notifyWithSelector:(SEL)selector {
+    NSHashTable *observers = self.observersHashTable;
+    for (id observer in observers) {
+        if ([observer respondsToSelector:selector]) {
+            [observer performSelector:selector withObject:self];
+        }
+    }
+}
+
+@end
