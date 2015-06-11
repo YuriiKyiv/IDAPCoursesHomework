06c4ab7cfe3fd40c82d937fdf4a090af8065b02f
YURII <trohumchyk@gmail.com>
2015-06-11 16:20:38 +0300

    ini new IOSStuding

---
+++ NewIOSStuding/NewIOSStuding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewIOSStuding
+//
+//  Created by YURII on 11.06.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewIOSStuding/NewIOSStuding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewIOSStuding
+//
+//  Created by YURII on 11.06.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
